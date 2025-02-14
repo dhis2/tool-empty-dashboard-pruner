2be97000e1b84b6e15a71ec6ddc9d670d40eb1ae
Jason Pickering <jason.p.pickering@gmail.com>
2025-02-14 16:59:28 +0100

    Integrate stale dashboards

--- src/app.js
+++ src/app.js
@@ -5,21 +5,49 @@ import "./css/header.css";
 import "./css/style.css";
 import DataTable from "datatables.net";
 import "datatables.net-dt/css/dataTables.dataTables.css";
-import { baseUrl, d2Delete, d2Get, performPostAndGet } from "./js/d2api.js";
+import { baseUrl, d2Delete, d2Get, d2PostJson, performPostAndGet } from "./js/d2api.js";
 window.DataTable = DataTable;
 
 function getContextPath() {
     var ctx = window.location.pathname.substring(0, window.location.pathname.indexOf("/", 1));
-    console.log("Context path: " + ctx);
     if (ctx == "/api") return "";
     return ctx;
 }
 
-const check_code = "dashboards_no_items";
+const checks = [{ "dashboards_no_items": "Dashboards with no items" },
+{ "dashboards_not_viewed_one_year": "Dashboards not viewed in one year" }];
+
+function renderTypeSelect() {
+    let html = "<select id='checkSelect' onchange='runDetails(this.value)'>";
+    checks.forEach((check) => {
+        const key = Object.keys(check)[0];
+        const value = check[key];
+        html += `<option value='${key}'>${value}</option>`;
+    });
+    html += "</select>";
+    return html;
+}
+
+async function getMetadataIntegrityChecks() {
+    try {
+        const endpoint = "dataIntegrity";
+        return await d2Get(endpoint);
+    } catch (error) {
+        console.error("Error getting metadata integrity checks:", error);
+    }
+}
+
+
+//Global variable to store the check code
+var check_code = "dashboards_no_items";
+//Global to store the checks metadata
+var checks_metadata = {};
+
+var me  = {};
 
 async function getDashboardProperties() {
     try {
-        const endpoint = "dashboards?fields=id,lastUpdated,access&paging=false";
+        const endpoint = "dashboards?fields=id,created,lastUpdated,access,sharing&paging=false";
         return await d2Get(endpoint);
     } catch (error) {
         console.error("Error getting dashboard properties:", error);
@@ -35,17 +63,32 @@ async function renderDetailsTable(detailsObject, dashboard_properties, user_is_s
     }
 
     detailsObject.issues.forEach((issue) => {
-        const dashboard = dashboard_properties.dashboards.find((dashboard) => dashboard.id === issue.id);
-        if (dashboard) {
-            const lastUpdated = new Date(dashboard.lastUpdated);
+        const getLastUpdatedDays = (dateString) => {
+            const date = new Date(dateString);
             const now = new Date();
-            const diffTime = Math.abs(now - lastUpdated);
-            const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-            issue.comment = diffDays;
+            const diffTime = Math.abs(now - date);
+            return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        };
+        const dashboard = dashboard_properties.dashboards.find((dashboard) => dashboard.id === issue.id);
+
+        if (issue.comment) {
+            issue.comment = getLastUpdatedDays(issue.comment);
         } else {
-            issue.comment = "Unknown";
+            if (dashboard) {
+                issue.comment = getLastUpdatedDays(dashboard.lastUpdated);
+            } else {
+                issue.comment = "Unknown";
+            }
+        }
+
+        if (dashboard) {
+            issue.created = getLastUpdatedDays(dashboard.created);
+        } else {
+            issue.created = "Unknown";
         }
     });
+
+
 
     //Determine if the dashboard has public access
     detailsObject.issues.forEach((issue) => {
@@ -57,15 +100,16 @@ async function renderDetailsTable(detailsObject, dashboard_properties, user_is_s
         }
     });
 
-    let html = "<div id='details_table'><h2>Empty dashboards</h2>";
-    html += "<h3>Dashboards with no content</h3>";
+    let html = "<div id='details_table'>";
     html += "<table id='details' class='display' width='100%'>";
-    html += "<thead><tr><th>Dashboard name</th><th>ID</th><th>Last updated (days ago)</th><th>Public access</th><th>Delete</th><th>Select</th></thead><tbody>";
+    html += "<thead><tr><th>Dashboard name</th><th>ID</th><th>Created (days ago)</th><th>Last updated (days ago)</th><th>Share with me</th><th>Access</th><th>Delete</th><th>Select</th></thead><tbody>";
     detailsObject.issues.forEach((issue) => {
         html += "<tr>";
         html += `<td>${issue.name}</td>`;
         html += `<td><a href='${baseUrl}/dhis-web-dashboard/#/${issue.id}' target='_blank'>${issue.id}</a></td>`;
+        html += `<td>${issue.created}</td>`;
         html += `<td>${issue.comment}</td>`;
+        html += `<td><button onclick='shareDashboardWithAlert("${issue.id}")'>Share</button></td>`;
         html += `<td>${issue.publicAccess}</td>`;
         html += `<td><button onclick='deleteSelectedDashboard("${issue.id}")'>Delete</button></td>`;
         html += `<td><input type='checkbox' class='dashboard-select' value='${issue.id}'></td>`;
@@ -81,18 +125,20 @@ async function renderDetailsTable(detailsObject, dashboard_properties, user_is_s
     return html;
 }
 
-async function runDetails(code) {
+async function runDetails(code, me) {
+    //Update the global variable
+    check_code = code;
     let user_is_super = false;
     const path = `dataIntegrity/details?checks=${code}`;
     try {
-        user_is_super = await checkUserIsSuperUser();
+        user_is_super = await checkUserIsSuperUser(me);
         const dashboard_properties = await getDashboardProperties();
         const data = await performPostAndGet(path);
         const name = Object.keys(data)[0];
         const this_check = data[name];
         const this_html = await renderDetailsTable(this_check, dashboard_properties, user_is_super);
         document.getElementById("detailsReport").innerHTML = this_html;
-        new DataTable("#details", { paging: true, searching: true, order: [[1, "desc"]] });
+        new DataTable("#details", { paging: true, searching: true, order: [[2, "desc"]] });
     } catch (error) {
         console.error("Error in runDetails:", error);
     }
@@ -100,6 +146,7 @@ async function runDetails(code) {
 
 async function deleteSelectedDashboard(uid) {
     if (confirm("Are you sure you want to delete this dashboard?")) {
+        await shareDashboardWithCurrentUser(uid);
         d2Delete(`dashboards/${uid}`).then(() => {
             alert("Dashboard deleted");
             runDetails(check_code);
@@ -121,6 +168,7 @@ async function deleteSelectedDashboards() {
                 const batch = selectedDashboards.slice(i, i + maxConcurrentRequests);
                 const results = await Promise.all(batch.map(async uid => {
                     try {
+                        await shareDashboardWithCurrentUser(uid);
                         await d2Delete(`dashboards/${uid}`);
                         return 200;
                     } catch (error) {
@@ -148,7 +196,6 @@ async function checkVersion() {
         var version = data.version.split(".")[1];
         //Only consider the digits
         version = parseInt(version.match(/\d+/)[0]);
-        console.log("DHIS2 version:", version);
         return version;
     } catch (error) {
         console.error("Error checking DHIS2 version:", error);
@@ -156,14 +203,29 @@ async function checkVersion() {
     }
 }
 
+async function getMe() {
+    try {
+        const data = await d2Get("me?fields=id,userRoles[id,name,authorities]");
+        return data;
+    } catch (error) {
+        console.error("Error getting current user:", error);
+    }
+}
+
 async function checkUserIsSuperUser() {
     try {
-        const data = await d2Get("me?fields=userRoles[id,name,authorities]");
-        const isSuperUser = data.userRoles.some(role => role.authorities.includes("ALL"));
-        return isSuperUser;
+        return me.userRoles.some(role => role.authorities.includes("ALL"));
     } catch (error) {
         console.error("Error checking user roles:", error);
         return false;
+    }
+}
+
+async function getCurrentUserUID() {
+    try {
+        return me.id;
+    } catch (error) {
+        console.error("Error getting current user UID:", error);
     }
 }
 
@@ -172,17 +234,59 @@ async function deleteAllEmptyDashboards() {
     deleteSelectedDashboards();
 }
 
+async function getDashboardSharing(dashboard_uid) {
+    try {
+        const sharing = await d2Get(`sharing?type=dashboard&id=${dashboard_uid}`);
+        return sharing;
+    } catch (error) {
+        console.error("Error getting dashboard sharing:", error);
+    }
+}
+
+async function  shareDashboardWithCurrentUser(dashboard_uid) {
+    try {
+        const sharing = await getDashboardSharing(dashboard_uid);
+        sharing.object.userAccesses.push({ id: me.id, access: "rw------" });
+        const resp = await d2PostJson(`sharing?type=dashboard&id=${dashboard_uid}`, sharing);
+        return resp;
+    } catch (error) {
+        console.error("Error sharing dashboard with user:", error);
+        alert("Error sharing dashboard with user");
+    }
+}
+
+async function shareDashboardWithAlert(dashboard_uid) {
+    if (confirm("Are you sure you want to share this dashboard with yourself?")) {
+        await shareDashboardWithCurrentUser(dashboard_uid);
+        alert("Dashboard shared with you");
+    }
+}   
+
 window.getContextPath = getContextPath;
 window.deleteAllEmptyDashboards = deleteAllEmptyDashboards;
 window.deleteSelectedDashboard = deleteSelectedDashboard;
 window.deleteSelectedDashboards = deleteSelectedDashboards;
 window.getDashboardProperties = getDashboardProperties;
+window.getDashboardSharing = getDashboardSharing;
+window.getCurrentUserUID = getCurrentUserUID;
+window.shareDashboardWithCurrentUser = shareDashboardWithCurrentUser;
+window.renderDetailsTable = renderDetailsTable;
+window.runDetails = runDetails;
+window.checkVersion = checkVersion;
+window.checkUserIsSuperUser = checkUserIsSuperUser;
+window.shareDashboardWithAlert = shareDashboardWithAlert;
+window.renderTypeSelect = renderTypeSelect;
 window.baseUrl = baseUrl;
 
 (async () => {
+    renderTypeSelect();
     const version = await checkVersion();
+    me = await getMe();
     const is_supported = version >= 39;
     if (is_supported) {
+        checks_metadata = await getMetadataIntegrityChecks();
+        //Render the select button for later
+        document.getElementById("typeofCheck").innerHTML = renderTypeSelect();
         runDetails(check_code);
     } else {
         document.getElementById("detailsReport").innerHTML = "<h2>Unsupported DHIS2 version</h2>";
